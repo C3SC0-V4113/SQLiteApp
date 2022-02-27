6ec6386e454cd1ddc4a109cb4f0206d42d8fd24b
CESCO <cescovalledev@gmail.com>
2022-02-26 19:54:59 -0600

    Funciona con dos tablas no relacionadas

--- app/src/main/java/edu/udb/sqliteapp/Personas.java
+++ app/src/main/java/edu/udb/sqliteapp/Personas.java
@@ -1,9 +1,9 @@
 package edu.udb.sqliteapp;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.content.ContentValues;
-import android.content.Intent;
 import android.database.Cursor;
-import android.database.sqlite.SQLiteConstraintException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
@@ -11,16 +11,14 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-public class MainActivity extends AppCompatActivity {
+public class Personas extends AppCompatActivity {
 
     private EditText et1,et2,et3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_personas);
         et1=(EditText)findViewById(R.id.et1);
         et2=(EditText)findViewById(R.id.et2);
         et3=(EditText)findViewById(R.id.et3);
@@ -38,16 +36,16 @@ public class MainActivity extends AppCompatActivity {
         ContentValues registro = new ContentValues();
 
         registro.put("codigo", cod);
-        registro.put("descripcion", descri);
-        registro.put("precio", pre);
+        registro.put("nombres", descri);
+        registro.put("edad", pre);
 
         try {
-            bd.insertOrThrow("articulos", null, registro);
+            bd.insertOrThrow("personas", null, registro);
             bd.close();
             et1.setText("");
             et2.setText("");
             et3.setText("");
-            Toast.makeText(this, "Se cargaron los datos del artículo",Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Se cargaron los datos de la persona",Toast.LENGTH_SHORT).show();
         } catch (SQLiteException e) {
             Toast.makeText(this, "ERROR!! No se cargaron los datos del artículo" + e.getMessage(),Toast.LENGTH_SHORT).show();
         }
@@ -58,12 +56,12 @@ public class MainActivity extends AppCompatActivity {
         AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"administracion", null, 1);
         SQLiteDatabase bd = admin.getWritableDatabase();
         String cod = et1.getText().toString();
-        Cursor fila = bd.rawQuery("select descripcion,precio from articulos where codigo=" + cod, null);
+        Cursor fila = bd.rawQuery("select nombres,edad from personas where codigo=" + cod, null);
         if (fila.moveToFirst()) {
             et2.setText(fila.getString(0));
             et3.setText(fila.getString(1));
         } else
-            Toast.makeText(this, "No existe un artículo con dicho código",
+            Toast.makeText(this, "No existe una persona con dicho código",
                     Toast.LENGTH_SHORT).show();
         bd.close();
     }
@@ -72,12 +70,12 @@ public class MainActivity extends AppCompatActivity {
         AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"administracion", null, 1);
         SQLiteDatabase bd = admin.getWritableDatabase();
         String descri = et2.getText().toString();
-        Cursor fila = bd.rawQuery("select codigo,precio from articulos where descripcion='" + descri +"'", null);
+        Cursor fila = bd.rawQuery("select codigo,edad from personas where nombres='" + descri +"'", null);
         if (fila.moveToFirst()) {
             et1.setText(fila.getString(0));
             et3.setText(fila.getString(1));
         } else
-            Toast.makeText(this, "No existe un artículo con dicha descripción",
+            Toast.makeText(this, "No existe una persona con dicha nombre",
                     Toast.LENGTH_SHORT).show();
         bd.close();
     }
@@ -86,16 +84,16 @@ public class MainActivity extends AppCompatActivity {
         AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,"administracion", null, 1);
         SQLiteDatabase bd = admin.getWritableDatabase();
         String cod= et1.getText().toString();
-        int cant = bd.delete("articulos", "codigo=" + cod, null);
+        int cant = bd.delete("personas", "codigo=" + cod, null);
         bd.close();
         et1.setText("");
         et2.setText("");
         et3.setText("");
         if (cant == 1)
-            Toast.makeText(this, "Se borró el artículo con dicho código",
+            Toast.makeText(this, "Se borró la persona con dicho código",
                     Toast.LENGTH_SHORT).show();
         else
-            Toast.makeText(this, "No existe un artículo con dicho código",
+            Toast.makeText(this, "No existe una persona con dicho código",
                     Toast.LENGTH_SHORT).show();
     }
 
@@ -107,20 +105,19 @@ public class MainActivity extends AppCompatActivity {
         String pre = et3.getText().toString();
         ContentValues registro = new ContentValues();
         registro.put("codigo", cod);
-        registro.put("descripcion", descri);
-        registro.put("precio", pre);
-        int cant = bd.update("articulos", registro, "codigo=" + cod, null);
+        registro.put("nombres", descri);
+        registro.put("edad", pre);
+        int cant = bd.update("personas", registro, "codigo=" + cod, null);
         bd.close();
         if (cant == 1)
             Toast.makeText(this, "se modificaron los datos", Toast.LENGTH_SHORT)
                     .show();
         else
-            Toast.makeText(this, "no existe un artículo con el código ingresado",
+            Toast.makeText(this, "no existe una persona con el código ingresado",
                     Toast.LENGTH_SHORT).show();
     }
 
     public void salir(View v){
-        Intent llamar=llamar=new Intent(this,Personas.class);
-        startActivity(llamar);
+        finish();
     }
 }
